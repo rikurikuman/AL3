33c68f05ff058202f108e851af90a9ce8943f177
rikurikuman <k021g1447@g.neec.ac.jp>
2022-04-08 11:40:02 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow("LE2A_07_カタグチ_リクト_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
